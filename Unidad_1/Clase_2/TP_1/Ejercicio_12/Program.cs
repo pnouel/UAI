9a39a71f67cdc058efd66e849905224dd7364415
Petro Nouel <petronouel@gmail.com>
2020-08-27 20:51:43 -0300

    ejercicio 12 terminado fecha actual

---
+++ Unidad_1/Clase_2/TP_1/Ejercicio_12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mostrar la fecha y hora actual.
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine("Fecha Actual es {0}",fecha.ToShortDateString());
+        }
+    }
+}
