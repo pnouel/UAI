0e8021f9a293ee56d1920c59cde28d4b3854a51d
Petro Nouel <petronouel@gmail.com>
2020-08-27 18:14:47 -0300

    Ejercicio 3 Perimetro Terminado

---
+++ Unidad_1/Clase_2/TP_1/WindowsFormsApp1/Ejercicio_03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ladoA = 0;
+            int ladoB = 0;
+            int ladoC = 0;
+            int perimetro = 0;
+            //Dados los lados de un triangulo calcular el perímetro.
+            //ingreso de datos
+            Console.WriteLine("Ingrese el lado A:");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado B");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado C");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+            //calculo de perimetro
+            perimetro = ladoA + ladoB + ladoC;
+            
+            Console.Clear();//limpiamos pantalla
+           
+            Console.WriteLine("El Perimetro del triangulo es : {0}",perimetro);//muestra de datos
+
+            Console.ReadKey();
+
+        }
+    }
+}
