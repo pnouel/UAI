f9ef7b38fd2636c58e42776fac744ad5687f0c3a
Petro Nouel <petronouel@gmail.com>
2020-08-27 20:35:58 -0300

    ejercico 11 terminado

---
+++ Unidad_1/Clase_2/TP_1/WindowsFormsApp1/Ejercicio_11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dada una frase informar la cantidad de caracteres que tiene.
+            string frase = null;
+            //Ingreso de datos
+            Console.WriteLine("Ingrese la Frase:");
+            frase = Console.ReadLine();
+            //mostras la longitud de la frase
+
+            Console.WriteLine("La frase tiene una longitud de : {0} caracteres",frase.Length);
+        }
+    }
+}
