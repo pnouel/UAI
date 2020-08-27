9d43c3fc65566651403cd6b4c7645b987bde132b
Petro Nouel <petronouel@gmail.com>
2020-08-27 20:24:17 -0300

    ejercicio 9 terminado concatenar

---
+++ Unidad_1/Clase_2/TP_1/WindowsFormsApp1/Ejercicio_09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dadas dos frases concatenarlas y mostrar el resultado. 
+            string frase1 = null;
+            string frase2 = null;
+            string fraseFinal = null;
+            //Ingreso de datos
+            Console.WriteLine("Ingrese primera Frase:");
+            frase1 = Console.ReadLine();
+            Console.WriteLine("Ingrese segunda Frase:");
+            frase2 = Console.ReadLine();
+
+            fraseFinal =String.Concat(frase1, frase2);//  contatenamos las dos frases
+
+            Console.WriteLine(fraseFinal);//mostramos resultado 
+        }
+    }
+}
