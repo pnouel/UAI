7373883188f8f91af80c076e7a9c0f48724de81a
Petro Nouel <petronouel@gmail.com>
2020-08-27 18:37:45 -0300

    Ejercicio  5 superficie de rectangulo

---
+++ Unidad_1/Clase_2/TP_1/WindowsFormsApp1/Ejercicio_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dados los datos necesarios de un Cuadrado calcular la superficie.
+            int lado = 0;
+            int superficie = 0;
+            // la superficie de un cuadrado es base al cuadrado
+            //Ingreso de datos
+            Console.WriteLine("Ingrese la base:");
+            lado = Convert.ToInt32(Console.ReadLine());
+
+            superficie = lado * lado;//Calculo de superficie
+
+            Console.Clear();//limpiamos pantalla
+
+            Console.WriteLine("La superficie del cuadrado es : {0}",superficie);//muestra de datos
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unidad_1/Clase_2/TP_1/WindowsFormsApp1/Ejercicio_05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dados los datos necesarios de un Rectángulo calcular la superficie.
+            // La superficie de un rectangulo se calcula con base * altura
+        int baseRec = 0;
+            int alturaRec = 0;
+            int superficeRec = 0;
+            //Ingreso de datos
+            Console.WriteLine("Ingrese la base:");
+            baseRec = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese la altura:");
+            alturaRec = Convert.ToInt32(Console.ReadLine());
+
+            superficeRec = baseRec * alturaRec;//calculo de superfice
+
+            Console.Clear();//limpiamos pantalla
+
+            Console.WriteLine("La superficie del Rectangulo es : {0}", superficeRec);//muestra de datos
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
